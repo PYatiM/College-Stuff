2480904cd721a69d7071fbc443f071a4e95e677b
P YATHIN MACHAIAH <p.yathinmachaiah18@gmail.com>
2025-10-12 19:04:19 +0530

    Update hill.c

--- CNS/LAB_PROGRAMS/hill.c
+++ CNS/LAB_PROGRAMS/hill.c
@@ -1,84 +1,55 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h> // Required for exit()
 
-int key[3][3];
-int invKey[3][3];
+// Global key matrices
+int key[3][3], invKey[3][3];
 
-int determinant(int m[3][3]) {
-    int det = m[0][0]*(m[1][1]*m[2][2] - m[1][2]*m[2][1])
-            - m[0][1]*(m[1][0]*m[2][2] - m[1][2]*m[2][0])
-            + m[0][2]*(m[1][0]*m[2][1] - m[1][1]*m[2][0]);
-    det %= 26;
-    if (det < 0) det += 26;
-    return det;
-}
+// Helper for correct modulo 26 arithmetic
+int mod(int a) { return (a % 26 + 26) % 26; }
 
+// Finds modular multiplicative inverse of a under modulo 26
 int modInverse(int a) {
-    a %= 26;
     for (int x = 1; x < 26; x++)
-        if ((a * x) % 26 == 1)
-            return x;
+        if (mod(a * x) == 1) return x;
     return -1;
 }
 
-void inverseMatrix(int m[3][3], int inv[3][3]) {
-    int det = determinant(m);
-    int invDet = modInverse(det);
+// Calculates determinant of a 3x3 matrix
+int determinant(int m[3][3]) {
+    int det = m[0][0] * (m[1][1] * m[2][2] - m[1][2] * m[2][1]) -
+              m[0][1] * (m[1][0] * m[2][2] - m[1][2] * m[2][0]) +
+              m[0][2] * (m[1][0] * m[2][1] - m[1][1] * m[2][0]);
+    return mod(det);
+}
+
+// Calculates the inverse of the global key matrix
+void getInverse() {
+    int invDet = modInverse(determinant(key));
     if (invDet == -1) {
         printf("Key matrix is not invertible mod 26!\n");
-        return;
+        exit(1);
     }
-
-    int adj[3][3];
-    adj[0][0] =  (m[1][1]*m[2][2] - m[1][2]*m[2][1]);
-    adj[0][1] = -(m[0][1]*m[2][2] - m[0][2]*m[2][1]);
-    adj[0][2] =  (m[0][1]*m[1][2] - m[0][2]*m[1][1]);
-    adj[1][0] = -(m[1][0]*m[2][2] - m[1][2]*m[2][0]);
-    adj[1][1] =  (m[0][0]*m[2][2] - m[0][2]*m[2][0]);
-    adj[1][2] = -(m[0][0]*m[1][2] - m[0][2]*m[1][0]);
-    adj[2][0] =  (m[1][0]*m[2][1] - m[1][1]*m[2][0]);
-    adj[2][1] = -(m[0][0]*m[2][1] - m[0][1]*m[2][0]);
-    adj[2][2] =  (m[0][0]*m[1][1] - m[0][1]*m[1][0]);
-
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
-            inv[i][j] = adj[j][i] * invDet;
-            inv[i][j] %= 26;
-            if (inv[i][j] < 0) inv[i][j] += 26;
-        }
-}
-
-void hillEncrypt(char text[]) {
-    int len = strlen(text);
-    for (int i = 0; i < len; i += 3) {
-        int vector[3], result[3];
-        for (int j = 0; j < 3; j++)
-            vector[j] = text[i + j] - 'A';
-
-        for (int j = 0; j < 3; j++) {
-            result[j] = 0;
-            for (int k = 0; k < 3; k++)
-                result[j] += key[j][k] * vector[k];
-            result[j] %= 26;
-            text[i + j] = result[j] + 'A';
+            int cofactor = key[(j + 1) % 3][(i + 1) % 3] * key[(j + 2) % 3][(i + 2) % 3] -
+                           key[(j + 1) % 3][(i + 2) % 3] * key[(j + 2) % 3][(i + 1) % 3];
+            invKey[i][j] = mod(((i + j) % 2 == 0 ? 1 : -1) * cofactor * invDet);
         }
     }
 }
 
-void hillDecrypt(char text[]) {
+// Encrypts or decrypts text using a given matrix
+void transform(char text[], int m[3][3]) {
     int len = strlen(text);
     for (int i = 0; i < len; i += 3) {
-        int vector[3], result[3];
-        for (int j = 0; j < 3; j++)
-            vector[j] = text[i + j] - 'A';
-
+        int res[3] = {0};
         for (int j = 0; j < 3; j++) {
-            result[j] = 0;
             for (int k = 0; k < 3; k++)
-                result[j] += invKey[j][k] * vector[k];
-            result[j] %= 26;
-            text[i + j] = result[j] + 'A';
+                res[j] += m[j][k] * (text[i + k] - 'A');
         }
+        for (int j = 0; j < 3; j++)
+            text[i + j] = mod(res[j]) + 'A';
     }
 }
 
@@ -89,15 +60,15 @@ int main() {
         for (int j = 0; j < 3; j++)
             scanf("%d", &key[i][j]);
 
-    inverseMatrix(key, invKey);
+    getInverse();
 
     printf("Enter text (multiple of 3 letters, uppercase A-Z): ");
     scanf("%s", text);
 
-    hillEncrypt(text);
+    transform(text, key);
     printf("Encrypted text: %s\n", text);
 
-    hillDecrypt(text);
+    transform(text, invKey);
     printf("Decrypted text: %s\n", text);
 
     return 0;
